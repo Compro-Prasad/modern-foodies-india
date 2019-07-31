65db9ed2e92e993f73a8f0814f754411e80f5b5d
Raj-kishore <raj.maharana.94@gmail.com>
2019-07-31 23:09:11 +0530

    foodposter part is pending because of version mismatch issues while integrating angular tabs routing

--- src/app/foodposter/foodposter.module.ts
+++ src/app/foodposter/foodposter.module.ts
@@ -8,6 +8,21 @@ import { IonicModule } from '@ionic/angular';
 import { FoodposterPage } from './foodposter.page';
 
 const routes: Routes = [
+  { // follow this https://github.com/ionic-team/ionic/blob/master/CHANGELOG.md#angular-tabs
+    path: 'tabs',
+    component: FoodposterPage,
+    children:[
+        { path: 'tab1',
+        children: [
+                    {
+                      path: '',
+                      loadChildren: '../tab1/tab1.module#Tab1PageModule'
+                    }
+                  ]
+      },
+        // { path: 'tab2', loadChildren: '../tab2/tab2.module#Tab2PageModule' },
+    ]
+  },
   {
     path: '',
     component: FoodposterPage

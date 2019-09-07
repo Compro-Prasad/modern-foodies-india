2bea168c3ad95ac537b5b81eb1b49d8821e281a5
Raj-kishore <raj.maharana.94@gmail.com>
2019-09-07 11:09:11 +0530

    fapi4 generated with more spring security configuration

--- foodali-webapi/src/main/java/com/example/demo/WebSecurityConfig.java
+++ foodali-webapi/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -13,20 +13,31 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
  
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-//        http.cors().and()
-//            //other config
+	@Override
+    protected void configure(HttpSecurity http) throws Exception{
+		//disable csrf to make post request
+        http.cors().and().csrf().disable();
     }
  
-    @Bean
-    CorsConfigurationSource corsConfigurationSource()
-    {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://phorons.com"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE","PUT"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource()
+//    {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("https://phorons.com"));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE","PUT"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+	 @Bean
+	    CorsConfigurationSource corsConfigurationSource() {
+	        CorsConfiguration configuration = new CorsConfiguration();
+	        configuration.setAllowedOrigins(Arrays.asList("*"));
+	        configuration.setAllowedMethods(Arrays.asList("*"));
+	        configuration.setAllowedHeaders(Arrays.asList("*"));
+	        configuration.setAllowCredentials(true);
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", configuration);
+	        return source;
+	    }
 }

db8687af97a8cdd72248270c318fcf430d5ba4fa
Raj-kishore <raj.maharana.94@gmail.com>
2019-09-07 10:00:49 +0530

    rebuild jar

--- foodali-webapi/src/main/java/com/example/demo/DemoApplication.java
+++ foodali-webapi/src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,9 @@ import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -57,14 +59,17 @@ public class DemoApplication {
 	}
 	
 	@Bean
-	public CorsFilter corsFilter() {
-	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    final CorsConfiguration config = new CorsConfiguration();
-	    config.setAllowCredentials(true);
-	    config.setAllowedOrigins(Collections.singletonList("*"));
-	    config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
-	    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
-	    source.registerCorsConfiguration("/**", config);
-	    return new CorsFilter(source);
-	}
+	public FilterRegistrationBean simpleCorsFilter() {  
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();  
+	    CorsConfiguration config = new CorsConfiguration();  
+	    config.setAllowCredentials(true); 
+	    // *** URL below needs to match the Vue client URL and port ***
+	    config.setAllowedOrigins(Collections.singletonList("*")); 
+	    config.setAllowedMethods(Collections.singletonList("*"));  
+	    config.setAllowedHeaders(Collections.singletonList("*"));  
+	    source.registerCorsConfiguration("/**", config);  
+	    FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+	    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);  
+	    return bean;  
+	}   
 }
